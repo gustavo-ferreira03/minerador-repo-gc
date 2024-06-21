c6cdb10f433b86fe88fdffd27a979ff02fccc4bb
gustavo-ferreira03 <gustavo_cosme@id.uff.br>
2024-06-21 14:10:24 -0300

    get all commits

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from pydriller import Repository
 
 repo = Repository('https://github.com/ishepard/pydriller')
 for commit in repo.traverse_commits():
-    print(f"{commit.hash}")
-    for file in commit.modified_files:
-        print(f"\t{file.filename}")
+    print(f"{commit.hash} - {commit.author_date} - {commit.committer_date}")
+    commit.comm
