a333db037c5a8cb8560229f5ad19582c4dae864e
gustavo-ferreira03 <gustavo_cosme@id.uff.br>
2024-06-21 14:43:11 -0300

    .

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from pydriller import Repository
 repo = Repository('https://github.com/ishepard/pydriller')
 for commit in repo.traverse_commits():
     print(f"{commit.hash} - {commit.author_date} - {commit.committer_date}")
-    commit.comm
+    commit
